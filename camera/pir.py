498ecd4036309ae45147ca30fd6effc4d154e4f0
funnylaksa <ivan.lee.ch8@gmail.com>
2021-03-28 15:58:59 +0800

    add logic to handle false positives

--- camera/pir.py
+++ camera/pir.py
@@ -23,32 +23,66 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)  
 time.sleep(10)       
 
+global count
 count = 0
+triggered = False
+
+
+def capture():
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d_%H%M%S")
+    print("date and time =", dt_string)
+    logger.info("PIR Sensor triggered. Capturing! --- ")
+    logger.info("---")
+    
+    photo = "/home/pi/picam/mega_temp/{datetime}".format(datetime = dt_string)
+    os.system("raspistill -awb greyworld  -o {photo}.jpg -rot 180".format(photo=photo))
+    
+    #vid = "/home/pi/picam/mega_temp/{datetime}".format(datetime = dt_string)
+    #os.system("raspivid -awb greyworld -o {vid}.h264 -rot 180 -t 3000".format(vid=vid))
+    time.sleep(3)
+    #os.system("MP4Box -add {vid}.h264 {vid}.mp4".format(vid=vid))  
+    #os.system("rm {vid}.h264".format(vid=vid))
+    count = 0
+    logger.info("Capturing Ends")
+
+# Start here
 while True:
     count += 1
     sensor_value = GPIO.input(11)
-    logger.info("Not activated")
-    print ("Sensor value:'", sensor_value)    
+    
+    print ("Sensor value:", sensor_value)    
     if count > 600:
         dt = count/60
         logger.info("Not triggered for past {dt} mins".format(dt=dt))
-        #count = 0
     
-    if sensor_value:
-        now = datetime.now()
-        dt_string = now.strftime("%Y%m%d_%H%M%S")
-        print("date and time =", dt_string)
-        logger.info("PIR Sensor triggered. Capturing! --- ")
-        logger.info("---")
+    # 4 positive sensor reading in 9 counts -> capture footage 
+    if triggered:
+        if sensor_value:
+            sensor_value_count+=1
+            if sensor_value_count % 8 == 0:
+                time.sleep(4)
+            if sensor_value_count % 3 == 0:
+                capture()
+                trigger_count = 1
+        elif trigger_count == 9:
+            triggered = False
+            trigger_count = 0
+        else:
+            trigger_count += 1
+        print('trig_count, sensor_val_count:', trigger_count, sensor_value_count)    
+        logger.info("trig_count, sensor_val_count: {}, {}".format(trigger_count, sensor_value_count))
+    else:
+        logger.info("Not activated")
         
-        photo = "/home/pi/picam/mega_temp/{datetime}".format(datetime = dt_string)
-        os.system("raspistill -awb greyworld  -o {photo}.jpg -rot 180".format(photo=photo))
-
-        vid = "/home/pi/picam/mega_temp/{datetime}".format(datetime = dt_string)
-        #os.system("raspivid -awb greyworld -o {vid}.h264 -rot 180 -t 3000".format(vid=vid))
-        time.sleep(5)
-        #os.system("MP4Box -add {vid}.h264 {vid}.mp4".format(vid=vid))  
-        #os.system("rm {vid}.h264".format(vid=vid))
-        count = 0
-
+    # init trigger for 1st positive sensor reading
+    if sensor_value and triggered==False:
+        trigger_count = 1
+        sensor_value_count=1
+        triggered = True
+        capture()
+        print('trig_count, sensor_val_count:', trigger_count, sensor_value_count)
+        logger.info("trig_count, sensor_val_count: {}, {}".format(trigger_count, sensor_value_count))
     time.sleep(1)
+
+
--- camera/simple_pir.py
+++ camera/simple_pir.py
@@ -8,7 +8,29 @@ GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 
 time.sleep(10)
 
+triggered = False
 while True:
-    i=GPIO.input(11)
-    print(i)
+    sensor_val=GPIO.input(11)
+    print(sensor_val)
+ 
+    # 4 positive sensor reading in 9 counts -> capture footage 
+    if triggered:
+        if sensor_val:
+            sensor_val_count += 1
+            if sensor_val_count % 4==0:
+                print('trigger!')
+                trigger_count = 1
+        elif trigger_count == 9:
+            triggered = False
+            trigger_count = 0
+        else: 
+            trigger_count+=1    
+        print('trig_count, sensor_val_count:', trigger_count, sensor_val_count)
+ 
+    #init trigger for 1st positive sensor reading 
+    if sensor_val and triggered==False:
+        trigger_count = 1
+        triggered = True
+        sensor_val_count = 1
+        print('trig_count, sensor_val_count:', trigger_count, sensor_val_count)
     time.sleep(1)
